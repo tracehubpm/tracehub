7b74ea589ad5e1f4e711baeed57b66d01d01fbd5
h1alexbel <hialexbel@gmail.com>
2024-01-31 16:26:44 +0300

    feat(#82): exclusion, tests, new puzzles

--- src/test/java/git/tracehub/tk/TkGitHubTest.java
+++ src/test/java/git/tracehub/tk/TkGitHubTest.java
@@ -28,11 +28,13 @@ import com.jcabi.github.Repos;
 import com.jcabi.github.mock.MkGithub;
 import git.tracehub.extensions.LocalGhProject;
 import git.tracehub.extensions.MkContribution;
+import git.tracehub.validation.XsErrors;
 import io.github.eocqrs.eokson.Jocument;
 import io.github.eocqrs.eokson.JsonOf;
 import org.cactoos.io.ResourceOf;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.takes.rq.RqFake;
 import org.takes.rs.RsPrint;
@@ -41,13 +43,16 @@ import org.takes.rs.RsPrint;
  * Test case for {@link TkGitHub}.
  *
  * @since 0.0.0
- * @checkstyle StringLiteralsConcatenationCheck (50 lines)
+ * @todo #82:30min Resolve code duplication in preparing project.
+ *  We should resolve code duplication inside blocks where we setup
+ *  MkGithub, each time and the same. We should consider creating JUnit
+ *  extension for this configuration and similar.
+ * @checkstyle StringLiteralsConcatenationCheck (200 lines)
  */
 final class TkGitHubTest {
 
     @Test
     void returnsOkOnRequest() throws Exception {
-        final String expected = "Thanks h1alexbel/test for GitHub webhook";
         final MkGithub github = new MkGithub();
         final Repo repo = github.users().add("h1alexbel")
             .github()
@@ -59,7 +64,7 @@ final class TkGitHubTest {
             "label: Update License year to 2024\ndescription:"
             + " test description\ncost: 20 minutes\nrole: DEV"
         ).value();
-        new LocalGhProject("github/projects/single-backed.yml", repo).value();
+        new LocalGhProject("yml/projects/single-backed.yml", repo).value();
         final String response = new RsPrint(
             new TkGitHub(github, "master").act(
                 new RqFake(
@@ -75,6 +80,154 @@ final class TkGitHubTest {
                 )
             )
         ).printBody();
+        final String expected = "Thanks h1alexbel/test for GitHub webhook";
+        MatcherAssert.assertThat(
+            "Response %s does not match with expected format %s"
+                .formatted(response, expected),
+            response,
+            new IsEqual<>(
+                expected
+            )
+        );
+    }
+
+    /**
+     * Test case for appending project error if there is no ID
+     * in YAML document.
+     *
+     * @throws Exception if something went wrong.
+     * @todo #82:25min Do not throw exception if Project ID is NULL.
+     *  We should turn this into an error (inside {@link git.tracehub.validation.ProjectValidation})
+     *  and add it after {@link XsErrors}. But not crash a program.
+     *  For now {@link git.tracehub.agents.github.GhProject}
+     *  terminates the execution since project.identity() is NULL.
+     */
+    @Test
+    @Disabled
+    void appendsErrorsIfThereIsNoId() throws Exception {
+        final MkGithub github = new MkGithub();
+        final Repo repo = github.users().add("h1alexbel")
+            .github()
+            .repos()
+            .create(new Repos.RepoCreate("test", false));
+        new MkContribution(
+            repo,
+            ".trace/jobs/fix-me.yml",
+            "label: Update License year to 2024\ndescription:"
+            + " test description\ncost: 20 minutes\nrole: DEV"
+        ).value();
+        new LocalGhProject("yml/projects/--no-id.yml", repo).value();
+        final String response = new RsPrint(
+            new TkGitHub(github, "master").act(
+                new RqFake(
+                    "POST",
+                    "/",
+                    new Jocument(
+                        new JsonOf(
+                            new ResourceOf(
+                                "github/hooks/mock.json"
+                            ).stream()
+                        )
+                    ).pretty()
+                )
+            )
+        ).printBody();
+        final String expected = "project.yml does not contain ID of the project";
+        MatcherAssert.assertThat(
+            "Response %s does not match with expected format %s"
+                .formatted(response, expected),
+            response,
+            new IsEqual<>(
+                expected
+            )
+        );
+    }
+
+    @Test
+    void returnsBadRequest() throws Exception {
+        final MkGithub github = new MkGithub();
+        final Repo repo = github.users().add("h1alexbel")
+            .github()
+            .repos()
+            .create(new Repos.RepoCreate("test", false));
+        new MkContribution(
+            repo,
+            ".trace/jobs/fix-me.yml",
+            "label: Update License year to 2024\ndescription:"
+            + " test description\ncost: 20 minutes\nrole: DEV"
+        ).value();
+        new LocalGhProject("yml/projects/--to-xml.yml", repo).value();
+        final String response = new RsPrint(
+            new TkGitHub(github, "master").act(
+                new RqFake(
+                    "POST",
+                    "/",
+                    new Jocument(
+                        new JsonOf(
+                            new ResourceOf(
+                                "github/hooks/mock.json"
+                            ).stream()
+                        )
+                    ).pretty()
+                )
+            )
+        ).printBody();
+        final String expected =
+            "`project.yml` document contains errors:"
+            + "\nProject must have exactly one Architect."
+            + "\nAt least one performer must have the DEV role.";
+        MatcherAssert.assertThat(
+            "Response %s does not match with expected format %s"
+                .formatted(response, expected),
+            response,
+            new IsEqual<>(
+                expected
+            )
+        );
+    }
+
+    /**
+     * Test case for warning suppression.
+     *
+     * @throws Exception if something went wrong
+     * @todo #82:30min Enable this test after warnings will be implemented.
+     *  Take a look at <a href="https://github.com/tracehubpm/tracehub/issues/44">this</a>
+     *  issue. Don't forget to remove this puzzle.
+     */
+    @Test
+    @Disabled
+    void returnsOkOnSuppressed() throws Exception {
+        final MkGithub github = new MkGithub();
+        final Repo repo = github.users().add("h1alexbel")
+            .github()
+            .repos()
+            .create(new Repos.RepoCreate("test", false));
+        new MkContribution(
+            repo,
+            ".trace/jobs/fix-me.yml",
+            "label: Update License year to 2024\ndescription:"
+            + " test description\ncost: 20 minutes\nrole: DEV"
+        ).value();
+        new LocalGhProject("yml/projects/pm-suppressed.yml", repo).value();
+        final String response = new RsPrint(
+            new TkGitHub(github, "master").act(
+                new RqFake(
+                    "POST",
+                    "/",
+                    new Jocument(
+                        new JsonOf(
+                            new ResourceOf(
+                                "github/hooks/mock.json"
+                            ).stream()
+                        )
+                    ).pretty()
+                )
+            )
+        ).printBody();
+        final String expected =
+            "`project.yml` document contains errors:"
+            + "\nProject must have exactly one Architect."
+            + "\nAt least one performer must have the DEV role.";
         MatcherAssert.assertThat(
             "Response %s does not match with expected format %s"
                 .formatted(response, expected),
