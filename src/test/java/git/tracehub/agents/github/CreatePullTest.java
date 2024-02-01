76d800a3818597e169ab953dc28a775cfc1d67fd
h1alexbel <hialexbel@gmail.com>
2024-02-01 10:34:34 +0300

    feat(#122): millis

--- src/test/java/git/tracehub/agents/github/CreatePullTest.java
+++ src/test/java/git/tracehub/agents/github/CreatePullTest.java
@@ -8,17 +8,12 @@ import com.jcabi.github.Reference;
 import com.jcabi.github.Repo;
 import com.jcabi.github.RtGithub;
 import com.jcabi.github.Tree;
-import java.io.IOException;
+import git.tracehub.KebabCase;
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZonedDateTime;
 import javax.json.Json;
-import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
-import org.cactoos.io.ResourceOf;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.mockito.ScopedMock;
 
 /**
  * Test case for {@link CreatePull}.
@@ -34,13 +29,16 @@ final class CreatePullTest {
             "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"
         ).repos().get(new Coordinates.Simple("h1alexbel/test"));
         final Issue.Smart submitted = new Issue.Smart(repo.issues().get(84));
-        // words... -> word-word2-word3
 
         final Branch master = repo.branches().find("master");
         final JsonObject json = master.commit().json();
         final String sha = json.getString("sha");
         final String tsha = json.getJsonObject("tree").getString("sha");
 
+        final String formatted = ".trace/jobs/%s.yml".formatted(
+            new KebabCase(submitted.title()).asString()
+        );
+        System.out.println(formatted);
         final Tree tree = repo.git().trees().create(
             Json.createObjectBuilder()
                 .add("base_tree", tsha)
@@ -49,16 +47,12 @@ final class CreatePullTest {
                     Json.createArrayBuilder()
                         .add(
                             Json.createObjectBuilder()
-                                .add(
-                                    "path",
-                                    ".trace/jobs/%s.yml".formatted(submitted.title())
-                                )
+                                .add("path", formatted)
                                 .add("mode", "100644")
                                 .add("type", "blob")
                                 .add("content", "")
                                 .build()
-                        )
-                        .build()
+                        ).build()
                 )
                 .build()
         );
@@ -72,36 +66,25 @@ final class CreatePullTest {
                         .add(sha)
                         .build()
                 )
-//                .add("branch", "test")
                 .build()
         );
         System.out.println(commit.json());
         final String cmsha = commit.sha();
         Reference reference;
-        if (repo.git().references().get("sync-3") == null) {
+        final long millis = Instant.now().toEpochMilli();
+        try {
+            repo.git().references().get("refs/heads/sync-%s".formatted(millis)).json();
+            reference = repo.git().references().get("refs/heads/sync-%s".formatted(millis));
+        } catch (final AssertionError er) {
             reference = repo.git().references().create(
-                "refs/heads/sync-3",
+                "refs/heads/sync-%s".formatted(millis),
                 cmsha
             );
-        } else {
-            reference = repo.git().references().get("sync-3");
         }
         repo.pulls().create(
-            "sync",
+            "sync(#%s)".formatted(submitted.number()),
             reference.ref(),
             "master"
         );
-//        System.out.println(reference.ref());
-
-//        final Reference reference = repo.git().references().create("sync-", tsha);
-
-
-//        repo.contents().create(
-//            Json.createObjectBuilder()
-//                .add("path", ".trace/jobs/%s.yml".formatted(submitted.title()))
-//                .add("message", "sync(#%s)".formatted(submitted.number()))
-//                .add("content", "")
-//                .build()
-//        ).path();
     }
 }
