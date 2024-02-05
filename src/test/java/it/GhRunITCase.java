4072435774cbcc051c951dd741ef1a5d6322d555
h1alexbel <hialexbel@gmail.com>
2024-02-05 14:13:23 +0300

    feat(#142): 80 secs

--- src/test/java/it/GhRunITCase.java
+++ src/test/java/it/GhRunITCase.java
@@ -51,7 +51,7 @@ final class GhRunITCase {
     void runs() throws Exception {
         final LogCaptor capt = LogCaptor.forClass(BaseRequest.class);
         new GhRun(new GhIdentity().value()).start();
-        Thread.currentThread().join(Duration.ofSeconds(65L).toMillis());
+        Thread.currentThread().join(Duration.ofSeconds(80L).toMillis());
         final List<String> infos = capt.getInfoLogs();
         final String invite = infos.get(1);
         final Pattern pattern = Pattern.compile(
